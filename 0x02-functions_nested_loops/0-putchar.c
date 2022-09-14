065382409053d67a69c953894a6f739b9e5567e6
HonourInnocent <flygirl4christ@gmail.com>
2022-09-13 20:24:03 -0700

    0._putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,7 @@
-#include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+#include<stdio.h>
 int main(void)
 {
-char *sh = "Holberton";
-while (*sh)
-{
-_putchar(*sh);
-sh++;
-}
-_putchar(n);
-return (0);
+	printf("_putchar \n");
+	getchar();
+	return 0;
 }
