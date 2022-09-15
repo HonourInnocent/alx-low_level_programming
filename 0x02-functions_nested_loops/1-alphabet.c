740d041ff4c2253779112619e2365d8898cc26cf
HonourInnocent <flygirl4christ@gmail.com>
2022-09-14 20:15:45 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
+#include "main.h"
+void print_alphabet(void)
 int main()
 {
     char c;
     for (c = 'a'; c <= 'z'; ++c)
         printf("%c ", c);
     return 0;
+    void print_alphabet(void)
+    {
+	    //print all the letters of the alphabet in lowercase
+    }
 }
 
