ef0ca3890d13ee8c8aa3bc97b04775274987251d
HonourInnocent <flygirl4christ@gmail.com>
2022-09-16 02:52:38 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,7 @@ int main()
     char c;
     for (c = 'a'; c <= 'z'; ++c)
         _putchar(c);
+    print_alphabet();
     return 0;
     {
             /**print all the letters of the alphabet in lowercase
