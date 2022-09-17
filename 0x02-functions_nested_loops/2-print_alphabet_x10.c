7c956d4b4a44647cfbfcf103f91b0ba6b4d00a76
HonourInnocent <flygirl4christ@gmail.com>
2022-09-17 06:17:19 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * print_alphabet_x10 -prints abc 10 times
+ *
+ * Return 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int i
+		for (i = 0; i <= 9; i++)
+		{
+
+			char c
+				for (c = 'a'; c <= 'z' c++)
+				{
+
+					_putchar(c);
+				}
+			_putchar('\n')
+		}
+	return(0);
+}
