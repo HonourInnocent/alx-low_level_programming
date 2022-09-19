05254e15c0759d057273e2bb88ef8781ebc2a63d
HonourInnocent <flygirl4christ@gmail.com>
2022-09-18 18:17:34 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
+/**
+ * main: Prints puts
+ *
+ * Return 0 (success)
+ */
+
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+
+puts ('\n');
 getchar();
 return 0;
 }
