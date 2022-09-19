d8a3a24db00080a2377bd6cf05161b3a13be900c
HonourInnocent <flygirl4christ@gmail.com>
2022-09-19 03:06:13 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
+int main(void)
 /**
- *Main - Entry point
  *
- * Return -0 (success)
- */
-
-int main (void)
-
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,");
