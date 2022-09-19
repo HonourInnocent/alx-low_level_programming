faa7060804b98c16da0afbd5798566877604957d
HonourInnocent <flygirl4christ@gmail.com>
2022-09-18 23:50:58 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,7 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	puts ('\n');
+	puts("\"Programming is like building a multilingual puzzle\n");
 	getchar();
 	return 0;
 }
