b39ac4a438434263226c38bc5216ca01fd4063f3
HonourInnocent <flygirl4christ@gmail.com>
2022-09-19 01:01:00 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,12 @@
  * Return -0 (success)
  */
 
-int main(void)
+int main (void)
+
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-printf("\n");
-getchar();
-return(0);
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("\n");
+	getchar();
+	return (0);
 }
