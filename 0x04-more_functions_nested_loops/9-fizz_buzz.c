26ca4af188d82faebb10ffb9be232a2cbb521b33
HonourInnocent <flygirl4christ@gmail.com>
2022-09-23 05:55:38 -0700

    Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,37 @@
 #include "main.h"
 
 /**
- * print_diagonal - draw a diagonal line
- * @n: number of times the '\' char is printed
- * Description: Can only use _putchar to print
- */
-void print_diagonal(int n)
+* main - fizzbuzz
+* Return: 0
+*/
+int main(void)
 {
-	int c, i;
+	int i = 1;
 
-	c = 0;
-
-	while (n > 0)
+	while (i <= 100)
 	{
-		i = c;
-		while (i > 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			_putchar(' ');
-			i--;
+			printf("FizzBuzz");
 		}
-		_putchar('\\');
-		_putchar('\n');
-		c++;
-		n--;
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+		i++;
 	}
-	if (c < 1)
-		_putchar('\n');
+	putchar('\n');
+	return (0);
 }
