07a6e393f4507cae92979544c5a95bdf28f340c7
HonourInnocent <flygirl4christ@gmail.com>
2022-09-29 16:52:44 -0700

    fix it

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,7 @@
  * Return - On success, return 1
  */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (*s == 0)
 	{
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,7 +5,7 @@
  *
  * Return - 0 (success)
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (s != 0)
 	{
